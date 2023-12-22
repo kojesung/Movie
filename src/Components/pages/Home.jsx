a3cf1b25aa06a4841960b79385b465da3aa2c29b
kojesung <js95112345@gmail.com>
2023-12-22 18:48:38 +0900

    fix

--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -1,6 +1,10 @@
 const Home = () => {
     return (
-        <div>Home</div>
+        <div>
+            <h1>
+                헤더의 메뉴를 클릭하면 해당 페이지로 이동합니다.
+            </h1>
+        </div>
     )
 }
 
